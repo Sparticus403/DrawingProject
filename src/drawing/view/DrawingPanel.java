11daee2b6ec80aba59aadbdfffc173564ed4d520
Sparticus403 <twil8980@740s-pl-118.local>
2017-06-02 08:45:36 -0600

    added some drawing features

--- src/drawing/view/DrawingPanel.java
+++ src/drawing/view/DrawingPanel.java
@@ -13,6 +13,11 @@ public class DrawingPanel extends JPanel
 	private DrawingController baseController;
 	private SpringLayout baseLayout;
 	private JButton rectangleButton;
+	private JButton triangleButton;
+	private JButton circleButton;
+	private JButton ellipseButton;
+	private JButton polygonButton;
+	private JButton resetButton;
 	private ShapePanel shapePanel;
 	
 	public DrawingPanel(DrawingController baseController)
@@ -20,8 +25,25 @@ public class DrawingPanel extends JPanel
 		super();
 		this.baseController = baseController;
 		shapePanel = new ShapePanel(baseController);
-		rectangleButton = new JButton("Draw rectangles");
+		rectangleButton = new JButton("Draw Rectangles");
+		triangleButton = new JButton("Draw Triangles");
+		circleButton = new JButton("Draw Circles");
+		ellipseButton = new JButton("Draw Ellipses");
+		polygonButton = new JButton("Draw Polygons");
+		resetButton = new JButton("Reset");
 		baseLayout = new SpringLayout();
+		baseLayout.putConstraint(SpringLayout.NORTH, resetButton, 6, SpringLayout.SOUTH, polygonButton);
+		baseLayout.putConstraint(SpringLayout.WEST, resetButton, 10, SpringLayout.WEST, polygonButton);
+		baseLayout.putConstraint(SpringLayout.NORTH, polygonButton, 6, SpringLayout.SOUTH, ellipseButton);
+		baseLayout.putConstraint(SpringLayout.WEST, polygonButton, 0, SpringLayout.WEST, rectangleButton);
+		baseLayout.putConstraint(SpringLayout.NORTH, ellipseButton, 6, SpringLayout.SOUTH, circleButton);
+		baseLayout.putConstraint(SpringLayout.WEST, ellipseButton, 0, SpringLayout.WEST, rectangleButton);
+		baseLayout.putConstraint(SpringLayout.NORTH, circleButton, 4, SpringLayout.SOUTH, triangleButton);
+		baseLayout.putConstraint(SpringLayout.WEST, circleButton, 0, SpringLayout.WEST, rectangleButton);
+		baseLayout.putConstraint(SpringLayout.NORTH, triangleButton, 6, SpringLayout.SOUTH, rectangleButton);
+		baseLayout.putConstraint(SpringLayout.WEST, triangleButton, 0, SpringLayout.WEST, rectangleButton);
+		baseLayout.putConstraint(SpringLayout.WEST, shapePanel, 164, SpringLayout.WEST, this);
+		baseLayout.putConstraint(SpringLayout.EAST, shapePanel, -10, SpringLayout.EAST, this);
 		
 		setupPanel();
 		setupLayout();
@@ -35,14 +57,17 @@ public class DrawingPanel extends JPanel
 		this.setMinimumSize(new Dimension(600, 600));
 		
 		this.add(rectangleButton);
+		this.add(triangleButton);
+		this.add(circleButton);
+		this.add(ellipseButton);
+		this.add(polygonButton);
+		this.add(resetButton);
 		this.add(shapePanel);
 	}
 	private void setupLayout()
 	{
 
 		baseLayout.putConstraint(SpringLayout.NORTH, rectangleButton, 10, SpringLayout.NORTH, this);
-		baseLayout.putConstraint(SpringLayout.WEST, shapePanel, 6, SpringLayout.EAST, rectangleButton);
-		baseLayout.putConstraint(SpringLayout.EAST, shapePanel, -10, SpringLayout.EAST, this);
 		baseLayout.putConstraint(SpringLayout.WEST, rectangleButton, 10, SpringLayout.WEST, this);
 		baseLayout.putConstraint(SpringLayout.NORTH, shapePanel, 10, SpringLayout.NORTH, this);
 		baseLayout.putConstraint(SpringLayout.SOUTH, shapePanel, 346, SpringLayout.NORTH, this);
@@ -54,6 +79,41 @@ public class DrawingPanel extends JPanel
 			public void actionPerformed(ActionEvent click)
 			{
 				shapePanel.addRectangles();
+			}
+		});
+		triangleButton.addActionListener(new ActionListener()
+		{
+			public void actionPerformed(ActionEvent click)
+			{
+				shapePanel.addTriangles();
+			}
+		});
+		circleButton.addActionListener(new ActionListener()
+		{
+			public void actionPerformed(ActionEvent click)
+			{
+				shapePanel.addCircles();
+			}
+		});
+		ellipseButton.addActionListener(new ActionListener()
+		{
+			public void actionPerformed(ActionEvent click)
+			{
+				shapePanel.addEllipses();
+			}
+		});
+		polygonButton.addActionListener(new ActionListener()
+		{
+			public void actionPerformed(ActionEvent click)
+			{
+				shapePanel.addPolygon();
+			}
+		});
+		resetButton.addActionListener(new ActionListener()
+		{
+			public void actionPerformed(ActionEvent click)
+			{
+				shapePanel.reset();
 			}
 		});
 	}
--- src/drawing/view/ShapePanel.java
+++ src/drawing/view/ShapePanel.java
@@ -1,5 +1,6 @@
 package drawing.view;
 
+import javax.imageio.ImageIO;
 import  javax.swing.*;
 import drawing.controller.DrawingController;
 
@@ -7,20 +8,47 @@ import java.awt.Color;
 import java.awt.Dimension;
 import java.util.ArrayList;
 import java.awt.Rectangle;
+import java.awt.*;
+import java.awt.geom.Ellipse2D;
+import java.awt.image.BufferedImage;
+import java.time.LocalDateTime;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.*;
 
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.time.LocalDateTime;
+import javax.imageio.ImageIO;
+
 public class ShapePanel extends JPanel
 {
 	private DrawingController baseController;
-	private ArrayList<Rectangle> rectangleList;
+	private ArrayList<Shape> rectangleList;
+	private ArrayList<Shape> triangleList;
+	private ArrayList<Shape> circleList;
+	private ArrayList<Shape> ellipseList;
+	private ArrayList<Shape> polygonList;
+	private ArrayList<ArrayList <Shape>> shapes;
+	
 	
 	public ShapePanel(DrawingController baseController)
 	{
 		super();
 		this.baseController = baseController;
-		rectangleList = new ArrayList<Rectangle>();
+		rectangleList = new ArrayList<Shape>();
+		circleList = new ArrayList<Shape>();
+		ellipseList = new ArrayList<Shape>();
+		triangleList = new ArrayList<Shape>();
+		polygonList = new ArrayList<Shape>();
+		shapes = new ArrayList<ArrayList <Shape>>();
+		
+		shapes.add(circleList);
+		shapes.add(rectangleList);
+		shapes.add(ellipseList);
+		shapes.add(triangleList);
+		shapes.add(polygonList);
 		
 		setupPanel();
 	}
@@ -55,26 +83,162 @@ public class ShapePanel extends JPanel
 		}
 		this.repaint();
 	}
+	public void addCircles()
+	{
+		if(circleList.size() > 500)
+		{
+			circleList.clear();
+		}
+		for(int index = 0; index < 30; index++)
+		{
+			int radius = (int) (Math.random() * 25) + 2;
+			int xCorner = (int) (Math.random() * this.getWidth() - 15);
+			int yCorner = (int) (Math.random() * this.getHeight() - 15);
+			Ellipse2D.Double current = new Ellipse2D.Double(xCorner, yCorner, radius, radius);
+			circleList.add(current);
+		}
+	}
+	public void addEllipses()
+	{
+		if(ellipseList.size() > 500)
+		{
+			ellipseList.clear();
+		}
+		for(int index = 0; index < 30; index++)
+		{
+			int xradius = (int) (Math.random() * 50) * 2 + 1;
+			int yradius = (int) (Math.random() * 50) + 2;
+			int xCorner = (int) (Math.random() * this.getWidth() - 15);
+			int yCorner = (int) (Math.random() * this.getHeight() - 15);
+			Ellipse2D.Double current = new Ellipse2D.Double(xCorner, yCorner, xradius, yradius);
+			ellipseList.add(current);
+		}
+	}
+	public void addTriangles()
+	{
+		if(triangleList.size() > 500)
+		{
+			triangleList.clear();
+		}
+		for(int index = 0; index < 30; index++)
+		{
+			int vertexCount = 3;
+			int [] xVertices = new int [vertexCount];
+			int [] yVertices = new int [vertexCount];
+			for(int vertex = 0; vertex < vertexCount; vertex++)
+			{
+				int xCorner = (int) (Math.random() * this.getWidth());
+				int yCorner = (int) (Math.random() * this.getHeight());
+				xVertices[vertex] = xCorner;
+				yVertices[vertex] = yCorner;
+			}
+			Polygon current = new Polygon(xVertices, yVertices, vertexCount);
+			triangleList.add(current);
+		}
+		this.repaint();
+	}
+	public void addPolygon()
+	{
+		if(polygonList.size() > 500)
+		{
+			polygonList.clear();
+		}
+		for(int index = 0; index < 30; index++)
+		{
+			int vertexCount = (int) (Math.random() * 7) + 4;
+			int [] xVertices = new int [vertexCount];
+			int [] yVertices = new int [vertexCount];
+			for(int vertex = 0; vertex < vertexCount; vertex++)
+			{
+				int xCorner = (int) (Math.random() * this.getWidth());
+				int yCorner = (int) (Math.random() * this.getHeight());
+				xVertices[vertex] = xCorner;
+				yVertices[vertex] = yCorner;
+			}
+			Polygon current = new Polygon(xVertices, yVertices, vertexCount);
+			polygonList.add(current);
+		}
+		this.repaint();
+	}
+	public void reset()
+	{
+		for(int index = 0; index < shapes.size(); index++)
+		{
+			shapes.get(index).clear();
+		}
+		this.setBackground(getRandomColor());
+		this.repaint();
+	}
 	
 	protected void paintComponent(Graphics graphics)
 	{
-		//this.setBackground(getRandomColor()); SEZURE WARNING
 		Graphics2D drawingGraphics = (Graphics2D) graphics;
-		for(Rectangle currentRectangle : rectangleList)
+		
+		for(ArrayList<Shape> currentList : shapes)
+			{
+			drawShapes(currentList, drawingGraphics);
+			}
+//		//this.setBackground(getRandomColor()); SEZURE WARNING
+//		Graphics2D drawingGraphics = (Graphics2D) graphics;
+//		for(Rectangle currentRectangle : rectangleList)
+//		{
+//			drawingGraphics.setColor(getRandomColor());
+//			int strokeWidth = (int) (Math.random() * 10) + 1;
+//			drawingGraphics.setStroke(new BasicStroke(strokeWidth));
+//			
+//			int randomness = (int) (Math.random() * 35);
+//			if(randomness % 5 == 0 || randomness % 7 == 0)
+//			{
+//				drawingGraphics.fill(currentRectangle);
+//			}
+//			else
+//			{
+//				drawingGraphics.draw(currentRectangle);
+//			}
+//		}
+	}
+	
+	private void drawShapes(ArrayList<Shape> shapeList, Graphics2D graphics)
+	{
+		for(Shape currentShape : shapeList)
 		{
-			drawingGraphics.setColor(getRandomColor());
-			int strokeWidth = (int) (Math.random() * 10) + 1;
-			drawingGraphics.setStroke(new BasicStroke(strokeWidth));
+			graphics.setColor(getRandomColor());
+			int strokeWidth = (int) (Math.random() * 10) +1;
+			graphics.setStroke(new BasicStroke(strokeWidth));
 			
 			int randomness = (int) (Math.random() * 35);
+			
 			if(randomness % 5 == 0 || randomness % 7 == 0)
 			{
-				drawingGraphics.fill(currentRectangle);
+				graphics.fill(currentShape);
+				graphics.setColor(getRandomColor());
+				graphics.draw(currentShape);
 			}
 			else
 			{
-				drawingGraphics.draw(currentRectangle);
+				graphics.draw(currentShape);
 			}
+		}
+	}
+	
+	public void savePanel()
+	{
+		BufferedImage panelImage = new BufferedImage(this.getSize().width, this.getSize().height, BufferedImage.TYPE_INT_ARGB);
+		Graphics fileGraphics = panelImage.getGraphics();
+		
+		Color background = new Color(getBackground().getRed(), getBackground().getGreen(), getBackground().getBlue());
+		fileGraphics.setColor(background);
+		fileGraphics.fillRect(0, 0, this.getSize().width, this.getSize().height);
+		this.printAll(fileGraphics);
+		fileGraphics.dispose();
+		
+		try
+		{
+			ImageIO.write(panelImage, "png", new File("My Modern art " + LocalDateTime.now().getHour() + "-" + LocalDateTime.now().getMinute() + ".png"));
+		}
+		catch(IOException error)
+		{
+			JOptionPane.showMessageDialog(this, "Save failed");
 		}
 	}
 
